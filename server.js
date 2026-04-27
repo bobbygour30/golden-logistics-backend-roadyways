2448520c087eece9ce413a6c2a10ea7edcd7168e
Rajat <rajat804@gmail.com>
2026-04-27 18:22:55 +0530

    server update

--- server.js
+++ server.js
@@ -3,25 +3,25 @@ import "dotenv/config";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import { promises as dns } from "dns";
-import authRoutes from "./routes/authRoutes.js";
+import authRoutes from './routes/authRoutes.js';
 
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const app = express();
 
-// Connect DB
+// Connect DB (IMPORTANT: call inside handler safe way in production)
 connectDB();
 
 app.use(express.json());
 
 const allowedOrigins = [
   "http://localhost:5173",
+  "https://roadways-frontend.vercel.app",
 ];
 
 const corsOptions = {
   origin: function (origin, callback) {
     if (!origin) return callback(null, true);
-
     if (allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
@@ -34,19 +34,22 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 
-// Routes
+
+
+app.use('/api/auth', authRoutes);
+
+
 app.get("/", (req, res) => {
   res.send("Backend is running!");
 });
-
-app.use("/api/auth", authRoutes);
-
-
 
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
+
 
 export default app;
